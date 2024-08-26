10e17bd7d2cfe2fa958e277a6dd7ea19f49f2ed2
Josep Crego <josep.crego@systrangroup.com>
2024-08-26 20:20:40 +0200

    msec format

--- translate-client.py
+++ translate-client.py
@@ -48,5 +48,5 @@ if __name__ == '__main__':
     tic = time.time()
     out = send_request_to_server(args.url, args.timeout, args.cfg, args.txt)
     print(out)
-    logging.info('client msec={:.2f}'.format(1000*(time.time()-tic)))
+    logging.info('client msec={1000*(time.time()-tic):.2f}')
 
